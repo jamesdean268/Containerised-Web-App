3dfb54b1af56fdf5bab822ed396b5fec39c20e5d
James Dean <jamesdean268@gmail.com>
2023-01-07 10:34:15 +1100

    Updated csproj to include dependencies, set up Dockerfile, and updated Startup.cs with dependencies

--- backend/Models/ItemContext.cs
+++ backend/Models/ItemContext.cs
@@ -2,12 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 // ------ LEARNING CODE ADDITION ------
 // Add the database context
-public class ItemContext : DbContext
+namespace backend
 {
-    public ItemContext(DbContextOptions<ItemContext> options)
-        : base(options)
+    public class ItemContext : DbContext
     {
-    }
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
 
-    public DbSet<Item> Items { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
 }
